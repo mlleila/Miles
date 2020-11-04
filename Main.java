c143c379cbb172f8a8c88e8f6fe80235d8b0e841
Leila Mamedova <m.lleila@bk.ru>
2020-11-04 20:56:48 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int miles = amount/20;
+        int totalMiles = miles;
+        System.out.println(totalMiles);
+    }
+}
